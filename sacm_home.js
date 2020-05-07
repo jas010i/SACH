5adc43b174e4eaf5593f04d959e1a64e71d2a0c3
jas010i <jas010i@users.noreply.github.com>
2020-05-07 17:59:38 -0500

    update

--- sacm_home.js
+++ sacm_home.js
@@ -27,19 +27,17 @@ function showSlides(n) {
   slides[slideIndex-1].style.display = "block";
   dots[slideIndex-1].className += " active";
   captionText.innerHTML = dots[slideIndex-1].alt;
-}
 
 var acc = document.getElementsByClassName("accordion");
 var i;
 }
 
+var acc = document.getElementsByClassName("accordion");
+var i;
+
 for (i = 0; i < acc.length; i++) {
   acc[i].addEventListener("click", function() {
-    /* Toggle between adding and removing the "active" class,
-    to highlight the button that controls the panel */
     this.classList.toggle("active");
-
-    /* Toggle between hiding and showing the active panel */
     var panel = this.nextElementSibling;
     if (panel.style.display === "block") {
       panel.style.display = "none";
