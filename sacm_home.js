5d57411f18cd465ecd16009682f15de9a3086fcf
jas010i <jas010i@users.noreply.github.com>
2020-08-10 18:06:06 -0500

    e

--- sacm_home.js
+++ sacm_home.js
@@ -109,3 +109,28 @@ for (var i = 0; i < btns.length; i++) {
     this.className += " active";
   });
 }
+
+function myFunction2() {
+  var x = document.getElementById("myDIV1");
+  if (x.style.display === "none") {
+    x.style.display = "block";
+  } else {
+    x.style.display = "none";
+  }
+}
+
+function myFunction3() {
+  var dots = document.getElementById("dots");
+  var moreText = document.getElementById("more");
+  var btnText = document.getElementById("myBtn3");
+
+  if (dots.style.display === "none") {
+    dots.style.display = "inline";
+    btnText.innerHTML = "Read more";
+    moreText.style.display = "none";
+  } else {
+    dots.style.display = "none";
+    btnText.innerHTML = "Read less";
+    moreText.style.display = "inline";
+  }
+}
